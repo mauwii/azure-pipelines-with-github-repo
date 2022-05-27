374f0c8b6a902c7cfe5f0acbbcfcae087b6e0166
Mauwii <mauwii@outlook.de>
2022-05-27 22:53:54 +0200

    single quotes are necesarry

--- scripts/New-BicepMatrix.ps1
+++ scripts/New-BicepMatrix.ps1
@@ -2,18 +2,17 @@
 $bicepDeployFolders = Get-ChildItem -Path $env:bicepDir -Name
 
 function New-BicepMatrix() {
-  Write-Host -NoNewline "##vso[task.setVariable variable=legs;isOutput=true]"
-  Write-Host -NoNewline "{"
+  Write-Host -NoNewline "##vso[task.setVariable variable=legs;isOutput=true]{"
   foreach ($folder in $bicepDeployFolders) {
     $i++
-    Write-Host -NoNewLine "$folder"
-    Write-Host -NoNewLine ":{"
-    Write-Host -NoNewLine "bicepDir: $env:bicepDir, "
-    Write-Host -NoNewline "bicepTemplateDir: $folder, "
-    Write-Host -NoNewline "bicepParameter: $env:bicepParameter, "
-    Write-Host -NoNewline "resourceGroupName: $env:resourceGroupName, "
-    Write-Host -NoNewline "azureSubscription: $env:azureSubscription, "
-    Write-Host -NoNewline "location: $env:location"
+    Write-Host -NoNewLine "`'$folder`':"
+    Write-Host -NoNewLine "{"
+    Write-Host -NoNewLine "`'bicepDir`':`'$env:bicepDir`', "
+    Write-Host -NoNewline "`'bicepTemplateDir`':`'$folder`', "
+    Write-Host -NoNewline "`'bicepParameter`':`'$env:bicepParameter`', "
+    Write-Host -NoNewline "`'resourceGroupName`':`'$env:resourceGroupName`', "
+    Write-Host -NoNewline "`'azureSubscription`':`'$env:azureSubscription`', "
+    Write-Host -NoNewline "`'location`':`'$env:location`'"
     Write-Host -NoNewline "}"
     if ($i -eq $bicepDeployFolders.Length) {
       Write-Host "}"

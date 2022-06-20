ca867343ba1991dee7f822f844258d79bcbc74db
Mauwii <mauwii@outlook.de>
2022-06-20 20:08:23 +0200

    Add Comments

--- scripts/cleanupautomation.ps1
+++ scripts/cleanupautomation.ps1
@@ -8,8 +8,10 @@ az login `
 # Get Subscriptions
 $AzSubscriptions = Get-AzSubscription
 
+# Get Current UTC-Time
 $currentUTCtime = (Get-Date).ToUniversalTime()
 
+# Iterate over subscriptions
 foreach ($AzSubscription in $AzSubscriptions) {
   Write-Host `
     -ForegroundColor Cyan `
@@ -17,30 +19,36 @@ foreach ($AzSubscription in $AzSubscriptions) {
     "Subscription:"
   Write-Host $AzSubscription.Name `n
 
+  # Set Context to current Subscription
   Set-AzContext `
     -Subscription $AzSubscription.Id
+  az account set `
+    --subscription $AzSubscription.Id
 
-  az account set --subscription $AzSubscription.Id
-
+  # Get Resource Groups in current Subscription
   $AzResourceGroups = Get-AzResourceGroup
 
+  # Iterate over Resource Groups
   foreach ($AzResourceGroup in $AzResourceGroups) {
-    $AzResources = Get-AzResource `
-      -ResourceGroupName $AzResourceGroup.ResourceGroupName
     Write-Host `
       -ForegroundColor Cyan `
       -NoNewline `
       "Resource Group: "
     Write-Host $AzResourceGroup.ResourceGroupName
 
+    # Get Resources in current Resource Group
+    $AzResources = Get-AzResource `
+      -ResourceGroupName $AzResourceGroup.ResourceGroupName
+
+    # Iterate over Resources in current Resource Group
     foreach ($AzResource in $AzResources) {
+      # Get Current Resource Age
       $AzCurrentResource = az resource list `
         --location $AzResource.Location `
         --name $AzResource.Name `
         --query "[].{Name:name, RG:resourceGroup, Created:createdTime, Changed:changedTime}" `
-        -o json |
-      ConvertFrom-Json
-      $AzResourceAge = $currentUTCtime - $AzCurrentResource.Created
+        -o json | ConvertFrom-Json
+      $AzResourceAge = $currentUTCtime - ($AzCurrentResource.Created).ToUniversalTime()
       Write-Host ($AzCurrentResource.Name, "current Age is", $AzResourceAge.Days, "Days")
     }
     Write-Host `

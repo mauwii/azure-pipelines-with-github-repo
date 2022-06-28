68ee3f9b40a039d46149a5e80899053f8bef017f
Matthias Wild <40327258+Mauwii@users.noreply.github.com>
2022-06-29 00:01:17 +0200

    Update/script/deploy mike (#75)
    * speed things up
    
    * use complete branchname, replace / by -
    
    * fix branchname variable
    
    * fix branchname
    
    * push mike only once

--- scripts/deploy-mike.sh
+++ scripts/deploy-mike.sh
@@ -3,21 +3,16 @@
 git config user.name "${BUILD_SOURCEVERSIONAUTHOR:-'Mauwii'}"
 git config user.email "${BUILD_REQUESTEDFOREMAIL:-'mauwii@mauwii.onmicrosoft.com'}"
 
-if [[ $ISPULLREQUEST = "True" ]]; then
-  branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH##*/}"
-else
-  branchname="${BUILD_SOURCEBRANCHNAME}"
-fi
+[[ $ISPULLREQUEST == "True" ]] && branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH}" ||  branchname="${BUILD_SOURCEBRANCH#refs\/heads\/}"
+branchname=${branchname//\//-}
 
-deleteVersion="$(mike list -j | jq '.[] | .version' | grep -m 1 '"${branchname}."')"
+deleteVersion="$(mike list -j | jq '.[] | .version' | grep -m 1 ${branchname}.)"
 
-if [[ -n $deleteVersion ]]; then
-  echo "deleting version ${deleteVersion} from mkdocs"
-  mike delete --push "${deleteVersion}"
-fi
+[[ -n "${deleteVersion}" ]] && mike delete "${deleteVersion}" || echo "no Version deployed yet for ${branchname}"
 
 if [[ $ISPULLREQUEST != "True" ]]; then
-  echo "deploying version ${branchname}.$BUILD_BUILDID to mkdocs"
+  echo "deploying version ${branchname}.$BUILD_BUILDID to gh-pages"
   mike deploy --title "${branchname}" --update-aliases "${branchname}.$BUILD_BUILDID" "${branchname}"
-  mike set-default --push main
 fi
+
+mike set-default --push main

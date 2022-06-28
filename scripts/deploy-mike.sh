1ecf6402a9efeaa21018b14339039b65a801792f
Mauwii <mauwii@outlook.de>
2022-06-28 23:12:03 +0200

    fix branchname variable

--- scripts/deploy-mike.sh
+++ scripts/deploy-mike.sh
@@ -3,11 +3,11 @@
 git config user.name "${BUILD_SOURCEVERSIONAUTHOR:-'Mauwii'}"
 git config user.email "${BUILD_REQUESTEDFOREMAIL:-'mauwii@mauwii.onmicrosoft.com'}"
 
-[[ $ISPULLREQUEST == "True" ]] && branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH//\//-}" ||  branchname="${BUILD_SOURCEBRANCHNAME//\//-}"
+[[ $ISPULLREQUEST == "True" ]] && branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH//\//-}" ||  branchname="${BUILD_SOURCEBRANCH//\//-}"
 
 deleteVersion="$(mike list -j | jq '.[] | .version' | grep -m 1 ${branchname}.)"
 
-[[ -n $deleteVersion ]] && mike delete --push "${deleteVersion}" || echo "no Version deployed yet for ${branchname}"
+[[ -n "${deleteVersion}" ]] && mike delete --push "${deleteVersion}" || echo "no Version deployed yet for ${branchname}"
 
 if [[ $ISPULLREQUEST != "True" ]]; then
   echo "deploying version ${branchname}.$BUILD_BUILDID to gh-pages"

816bd765824e5f3a5d7ce4d140c4625858515581
Mauwii <mauwii@outlook.de>
2022-06-28 20:01:52 +0200

    fix branchname for pull requests

--- scripts/deploy-mike.sh
+++ scripts/deploy-mike.sh
@@ -2,15 +2,26 @@
 
 git config user.name "${BUILD_SOURCEVERSIONAUTHOR:-mauwii}"
 git config user.email "${BUILD_REQUESTEDFOREMAIL:-'mauwii@mauwii.onmicrosoft.com'}"
-mike delete "$BUILD_SOURCEBRANCHNAME"
-deleteVersion=$(mike list | grep -m 1 ${BUILD_SOURCEBRANCHNAME})
+
+if [[ $ISPULLREQUEST = "True" ]]; then
+  branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH}"
+elif
+  branchname="${BUILD_SOURCEBRANCHNAME}"
+fi
+
+mike delete "${branchname}"
+
+deleteVersion=$(mike list | grep -m 1 ${branchname})
+
 if [[ -n $deleteVersion ]]; then
   deleteVersion=${deleteVersion##*\(}
   deleteVersion=${deleteVersion%\)*}
   mike delete $deleteVersion
 fi
+
 if [[ $ISPULLREQUEST != "True" ]]; then
-  mike deploy -t "$BUILD_SOURCEBRANCHNAME" --update-aliases "$BUILD_SOURCEBRANCHNAME.$BUILD_BUILDID" "$BUILD_SOURCEBRANCHNAME"
+  mike deploy -t "${branchname}" --update-aliases "${branchname}.$BUILD_BUILDID" "${branchname}"
   mike set-default main
 fi
+
 git push origin gh-pages

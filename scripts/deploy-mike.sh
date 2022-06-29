83a9b3fc3700f86d1b78369c23a0fa73a33df279
Matthias Wild <40327258+Mauwii@users.noreply.github.com>
2022-06-29 02:04:11 +0200

    Update/script/deploy mike add comments (#76)
    * add comments

--- scripts/deploy-mike.sh
+++ scripts/deploy-mike.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
+# configure git to make mike work
 git config user.name "${BUILD_SOURCEVERSIONAUTHOR:-'Mauwii'}"
 git config user.email "${BUILD_REQUESTEDFOREMAIL:-'mauwii@mauwii.onmicrosoft.com'}"
 
-[[ $ISPULLREQUEST == "True" ]] && branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH}" ||  branchname="${BUILD_SOURCEBRANCH#refs\/heads\/}"
-branchname=${branchname//\//-}
+# set branchname for mike deployment
+[[ $ISPULLREQUEST == "True" ]] \
+  && branchname="${SYSTEM_PULLREQUEST_SOURCEBRANCH}" \
+  || branchname="${BUILD_SOURCEBRANCH#refs/heads/}"
 
-deleteVersion="$(mike list -j | jq '.[] | .version' | grep -m 1 ${branchname}.)"
+# set versionname for mike deployment
+versionName=${branchname//\//-}
 
-[[ -n "${deleteVersion}" ]] && mike delete "${deleteVersion}" || echo "no Version deployed yet for ${branchname}"
+# find currently deployed version for this branch
+deleteVersion=$(mike list -j | jq '.[] | .version' | grep -m 1 ${versionName})
 
-if [[ $ISPULLREQUEST != "True" ]]; then
-  echo "deploying version ${branchname}.$BUILD_BUILDID to gh-pages"
-  mike deploy --title "${branchname}" --update-aliases "${branchname}.$BUILD_BUILDID" "${branchname}"
+# delete the previously deployed version
+if [[ -n "${deleteVersion}" ]]; then
+  mike delete ${deleteVersion//\"/}
 fi
 
-mike set-default --push main
+# if not pull request deploy currnent version, otherwise delete alias
+if [[ $ISPULLREQUEST != "True" ]]; then
+  echo "deploying version ${versionName}.$BUILD_BUILDID to gh-pages"
+  mike deploy \
+    --push \
+    --title "${versionName}" \
+    --update-aliases "${versionName}.$BUILD_BUILDID" "${versionName}"
+fi

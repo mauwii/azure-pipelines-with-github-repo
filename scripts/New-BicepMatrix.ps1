e56141f9a9527ddb7cb457fb0f94af6d85c745af
Matthias Wild <40327258+Mauwii@users.noreply.github.com>
2022-07-05 00:28:22 +0200

    Update/New-BiceepMatrix/add-rg-json (#86)
    * check if folder contains rg.json
    if found, use containing ResourceGroupName instead of env
    
    * add example with rg.json

--- scripts/New-BicepMatrix.ps1
+++ scripts/New-BicepMatrix.ps1
@@ -1,26 +1,48 @@
-# Reference: {'a':{'myvar':'A'}, 'b':{'myvar':'B'}}
-$bicepDeployFolders = Get-ChildItem -Path $env:bicepDir -Name
+# Gef Folders to deploy
+$bicepDeployFolders = (
+  Get-ChildItem `
+    -Path $env:bicepDir `
+    -Name
+)
 
+# Function to create the Matrix
 function New-BicepMatrix() {
+  # Begin to output the matrix json as variable
   Write-Host -NoNewline "##vso[task.setVariable variable=legs;isOutput=true]{"
   foreach ($folder in $bicepDeployFolders) {
     $i++
+
+    # if folder containes rg.json file, get ResourceGroupName from there, otherwise use environment
+    if (Test-Path -Path "${env:bicepDir}/${folder}/rg.json") {
+      $ResourceGroupName = (
+        Get-Content `
+          -LiteralPath "${env:bicepDir}/${folder}/rg.json" |
+        ConvertFrom-Json
+      ).ResourceGroupName
+    }
+    else {
+      $ResourceGroupName = $env:resourceGroupName
+    }
+
     Write-Host -NoNewLine "`'$folder`':"
     Write-Host -NoNewLine "{"
-    Write-Host -NoNewLine "`'bicepDir`':`'$env:bicepDir`', "
-    Write-Host -NoNewline "`'bicepTemplateDir`':`'$folder`', "
-    Write-Host -NoNewline "`'bicepParameter`':`'$env:bicepParameter`', "
-    Write-Host -NoNewline "`'resourceGroupName`':`'$env:resourceGroupName`', "
-    Write-Host -NoNewline "`'azureSubscription`':`'$env:azureSubscription`', "
+    Write-Host -NoNewLine "`'bicepDir`':`'$env:bicepDir`',"
+    Write-Host -NoNewline "`'bicepTemplateDir`':`'$folder`',"
+    Write-Host -NoNewline "`'bicepParameter`':`'$env:bicepParameter`',"
+    Write-Host -NoNewline "`'resourceGroupName`':`'$ResourceGroupName`',"
+    Write-Host -NoNewline "`'azureSubscription`':`'$env:azureSubscription`',"
     Write-Host -NoNewline "`'location`':`'$env:location`'"
     Write-Host -NoNewline "}"
+
+    # If last folder has been reached end output of matrix json variable, continue otherwise
     if ($i -eq $bicepDeployFolders.Length) {
       Write-Host "}"
     }
     else {
-      Write-Host -NoNewline ", "
+      Write-Host -NoNewline ","
     }
   }
 }
 
+# Function-Call
 New-BicepMatrix
